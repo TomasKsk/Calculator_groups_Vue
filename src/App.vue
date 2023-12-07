6b5482fa76563bd079756d4727e2d06116034dcd
Tomáš Krže <106106200+TomasKsk@users.noreply.github.com>
2023-12-07 02:17:22 +0100

    Update App.vue
    changed to access the globals via state.
    Added handleNumberClick logic

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="calc-main" class="calc-grid">
     <div class="calc-display">
-      <div id="menu-icon-place" class="storage-window" @click="handleMenu">{{ menuIcon }}</div>
-      <div class="calc-current">{{ calcDisp }}</div>
-      <div class="calc-mem">{{ calcMem }}</div>
-      <div><span class="save-button saveIcon" id="save-icon-place" @click="saveCalc">{{ saveIco }}</span></div>
+      <div id="menu-icon-place" class="storage-window" @click="handleMenu">{{ state.menuIcon }}</div>
+      <div class="calc-current">{{ state.calcDisp }}</div>
+      <div class="calc-mem">{{ state.calcMem }}</div>
+      <div><span class="save-button saveIcon" id="save-icon-place" @click="saveCalc">{{ state.saveIco }}</span></div>
     </div>
 
     <div class="calc-keys">
@@ -13,14 +13,14 @@
       <button v-for="num in [7, 8, 9, 4, 5, 6, 1, 2, 3, 0]" :key="num">{{ num }}</button> 
 
       <button class="op5" @click="handleDotClick">.</button>
-      <button id="AC-key" class="op6" @click="handleDel(delKey)">{{ delKey }}</button>
+      <button id="AC-key" class="op6" @click="handleDel(delKey)">{{ state.delKey }}</button>
       <button class="op7" @click="handleEqualClick">=</button>
     </div>
   </div>
 </template>
 
 <script>
-import { onMounted, onUnmounted } from 'vue';
+import { reactive, onMounted, onUnmounted } from 'vue';
 //import CalcMemory from '@/components/CalcMemory.vue';
 //import CalcDisplay from '@/components/CalcDisplay.vue';
 //import CalcKeys from '@/components/CalcKeys.vue';
@@ -29,22 +29,6 @@ const operandArr = ["÷", "x", "-", "+"];
 const dataTypes = ['header', 'number', 'comment', 'operator'];
 
 export default {
-  data() {
-    return {
-      calcStorage: {},
-      calcMem: [],
-    }
-  },
-  methods: {
-    handleNumberClick(num, e) {
-      console.log('ysy')
-    },
-    handleOperandClick(num, e) {
-
-    }
-
-  },
-
   watch: {
     calcStorage: {
       handler(newValue) {
@@ -54,8 +38,35 @@ export default {
     },
   },
   setup() {
+    const state = reactive({
+      calcStorage: {},
+      calcMem: [],
+      menuIcon: '≡',
+      saveIco: '',
+      calcDisp: '',
+      calcOp: '',
+      delKey: 'C',
+    });
+
     const handleNumberClick = (num, e) => {
-      console.log('ysy');
+      if (e.target.matches('button')) {
+        if (!state.calcMem.includes('=')) {
+          state.calcDisp += num;
+          if (state.calcOp !== '') {
+            state.calcOp = '';
+          }
+          if (state.delKey === 'CE') {
+            state.delKey = 'C';
+          }
+        } else {
+          if (state.delKey === 'CE') {
+            state.delKey = 'C';
+          }
+          state.calcDisp = '';
+          state.calcMem = [];
+          state.calcDisp += num;
+        }
+      }
     };
 
     const handleOperandClick = (num, e) => {
@@ -81,7 +92,7 @@ export default {
     });
 
     // Return the methods you want to expose to the template
-    return { handleNumberClick, handleOperandClick };
+    return { state, handleNumberClick, handleOperandClick };
   },
 }
 
