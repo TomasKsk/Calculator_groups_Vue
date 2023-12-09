f2a41f281d6a74f6194de1170669941d1ce3cb51
Tomáš Krže <106106200+TomasKsk@users.noreply.github.com>
2023-12-09 21:03:48 +0100

    Update App.vue
    div map loop to populate the calcStorage window with its items.

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="calc-main" class="calc-grid">
     <div class="calc-display">
-      <div id="menu-icon-place" class="storage-window" @click="handleMenu">{{ state.menuIcon }}</div>
+      <div id="menu-icon-place" class="storage-window">{{ state.menuIcon }}</div>
       <div class="calc-current">{{ state.calcDisp }}</div>
       <div class="calc-mem">{{ state.calcMem.join('') }}</div>
-      <div><span class="save-button saveIcon" id="save-icon-place" @click="saveCalc">{{ state.saveIco }}</span></div>
+      <div><span class="save-button saveIcon" id="save-icon-place">{{ state.saveIco }}</span></div>
     </div>
 
     <div class="calc-keys">
@@ -12,18 +12,43 @@
 
       <button v-for="num in [7, 8, 9, 4, 5, 6, 1, 2, 3, 0]" :key="num">{{ num }}</button> 
 
-      <button class="op5" @click="handleDotClick">.</button>
-      <button id="AC-key" class="op6" @click="handleDel(delKey)">{{ state.delKey }}</button>
-      <button class="op7" @click="handleEqualClick">=</button>
+      <button class="op5">.</button>
+      <button id="AC-key" class="op6">{{ state.delKey }}</button>
+      <button class="op7">=</button>
     </div>
+
+    <div id="calc-mem-storage" class="calc-mem-storage">
+      <div v-for="(item, key) in state.calcStorage" :key="key" class="storage-item" :id="key">
+        <div>
+          <h3>
+            <span class="editable" data-type="header" :data-idparent="key">{{ item.name }}</span>
+            <button class="delete-mem" data-type="deleteButton" :data-idparent="key">x</button>
+          </h3>
+        </div>
+        <div v-for="(value, index) in item.calculation" :key="index">
+          <template v-if="typeof value === 'number'">
+            <strong>
+              <span v-if="index === item.calculation.length - 1" :data-idparent="key" :data-index="index">{{ value }}</span>
+              <span v-else class="editable" data-type="number" :data-idparent="key" :data-index="index">{{ value }}</span>
+            </strong>
+            <span v-if="index !== item.calculation.length - 1" class="editable" data-type="comment" :data-idparent="key" :data-index="index">{{ item.comments[index] }}</span>
+            <button v-if="index === item.calculation.length - 1" data-type="addNum" :data-index="index" :data-idparent="key">Add</button>
+          </template>
+          <template v-else>
+            <span v-if="index === item.calculation.length - 2">{{ value }}</span>
+            <span v-else class="editable" data-type="operator" :data-idparent="key" :data-index="index">{{ value }}</span>
+          </template>
+        </div>
+      </div>
   </div>
+</div>
+
+  
 </template>
 
 <script>
 import { reactive, onMounted, onUnmounted } from 'vue';
-//import CalcMemory from '@/components/CalcMemory.vue';
-//import CalcDisplay from '@/components/CalcDisplay.vue';
-//import CalcKeys from '@/components/CalcKeys.vue';
+
 
 const operandArr = ["÷", "x", "-", "+"];
 const dataTypes = ['header', 'number', 'comment', 'operator'];
@@ -46,7 +71,6 @@ export default {
       calcDisp: '',
       calcOp: '',
       delKey: 'C',
-      calcStorage: {},
       calcMemCount: 0,
     });
 
@@ -119,7 +143,7 @@ export default {
       return temp;
     }
 
-    const handleEqualClick = () => {
+    const handleEqualClick = (e) => {
       if (state.calcDisp !== '' && state.calcMem.length > 1) {
         let tempMem = [...state.calcMem, state.calcDisp];
         let tempDisp = recalc(tempMem);
@@ -168,7 +192,7 @@ export default {
       if (sel === '≡' || sel === 'x') {
         let sel = document.querySelector('.calc-mem-storage')
         let condi = sel !== null;
-        state.menuIcon = state.menuIcon === 'x' ? '≡' : 'x';
+        state.menuIcon = state.menuIcon === '≡' ? 'x' : '≡';
         if (condi) return sel.classList.toggle('visible');
       }
     };
@@ -185,7 +209,7 @@ export default {
         handleDotClick();
       } else if (operandArr.includes(num) && numId !== 'menu-icon-place') {
         handleOperandClick(num, e);
-      } else if (num === '=') {
+      } else if (num === '=' && numId !== 'menu-icon-place') {
         handleEqualClick();
       } else if (['C', 'CE'].includes(num)) {
         handleDel(num);
